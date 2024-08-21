f6251dfbc3ed1addb4a578981cac6cea8ee107fc
ruskyvisky <numan_859@hotmail.com>
2024-08-21 08:02:54 +0300

    Swagger UI has been added to the inventory service.

---
+++ inventory-service/src/main/java/com/ruskyvisky/inventory_service/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package com.ruskyvisky.inventory_service.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI InventoryServiceAPI(){
+        return new OpenAPI().info(new Info()
+                .title("Inventory Service API")
+                .description("Inventory Service API for Ruskyvisky")
+                .version("v1.0.0"));
+    }
+}
